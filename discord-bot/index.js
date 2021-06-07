20adeae51cf128dab59f1952f46cbbb9caec4e28
Beatriz de Souza <biamoreirasouza6@gmail.com>
2021-06-07 18:00:14 -0300

    alterações

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -4,3 +4,37 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 client.login(config.BOT_TOKEN);
+
+const client = new Discord.Client();
+
+
+client.on("message", function(message) { 
+                                         
+});                                      
+
+client.login(config.BOT_TOKEN);
+
+const client = new Discord.Client();
+
+
+client.on("message", function(message) { 
+                                         
+});                                      
+
+client.login(config.BOT_TOKEN);
+
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+    
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+}                        
+});
